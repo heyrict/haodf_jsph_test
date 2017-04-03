e9b81f1275c4b47c1b1a240f2247e9ce1390b60c
heyrict <xiezh0831@126.com>
2017-04-03 23:01:08 +0800

    adding all-patient scraping codes

--- haodf_jsph_test_1.py
+++ haodf_jsph_test_1.py
@@ -30,6 +30,73 @@ class WebContainer(object):
     def xpath(self,expression):
         return self.find(expression,kind='xpath')
 
+##疗效及态度满意度
+sat_att = sat_eff = {'很不满意':1,'不满意':2,'一般':3,'满意':4,'很满意':5,'其他':0}
+##看病目的
+aim = {'治疗':3,'未填':0,'诊断':2,'其他':1,'咨询问题':4}
+##选择该医生的理由
+reason = {'网上评价':3,'医生推荐':2,'其他':1,'未填':0,'熟人推荐':4,}
+##本次挂号途径
+reservation = {'网络预约':3,'排队挂号':2,'其他':1,'未填':0}
+##目前病情状态
+status = {'有好转':3,'其他':1,'未见好转':2,'痊愈':4,'未填':0}
+##名称匹配
+namspc = {'看病目的':'aim','疗效':'sat_eff','态度':'sat_att','选择该医生就诊的理由':'reason','本次挂号途径':'reservation','目前病情状态':'status','本次看病费用总计':'cost'}
+
+def current_page_to_df(this_page):
+    global sat_att, sat_eff, aim, reason, reservation, status, namspc, temp
+    curpatdf = DataFrame(columns=['lblix','docix','time','aim','reason','sat_eff','sat_att','reservation','status','cost'])
+    
+    for pat in this_page.xpath('//table[@class="doctorjy"]'):
+        curpat = pd.Series({'lblix':lblix,'docix':docix},index=['lblix','docix','time','aim','reason','sat_eff','sat_att','reservation','status','cost'])
+        #Time Processing
+        t = pat.xpath('.//td[contains(@style,"text-align:right;")]/text()').extract_first()
+        try: t = parser.parse(t[3:]).strftime(r'%Y-%m-%d')
+        except: t = np.nan
+        curpat['time'] = t
+
+        #dlemd Processing
+        patbriefinfo = pat.xpath('.//td[@class="dlemd"]')
+        ##aim
+        for i in patbriefinfo.xpath('.//td[@colspan="3"]/span/text()').extract():
+            t = i.split('：')
+            if t[0] in namspc.keys(): 
+                curaim = [eval(namspc[t[0]])[i] if i in eval(namspc[t[0]]) else 1 for i in t[1].split('、')] 
+                curpat[namspc[t[0]]] = str(curaim)
+
+        ##satisfaction
+        t = patbriefinfo.xpath('./table/tbody/tr')[-1].xpath('./td')
+        for i in t[:2]:
+            try:
+                text = i.xpath('text()').extract_first()
+                if text: temp = namspc[text[:-1]]
+                try:
+                    degr = eval(temp)[i.xpath('span/text()').extract_first()]
+                except:
+                    degr = 0
+                curpat[temp] = degr
+            except Exception as e:
+                print('Exception raised: %s'%e)
+                continue
+        
+        #tbody processing
+        patadditinfo = pat.xpath('.//tbody//td[@valign="top"][@height="40px"]')
+        for i in patadditinfo.xpath('div'):
+            try:
+                if not i.xpath('span/text()').extract_first(): continue
+                temp = namspc[i.xpath('span/text()').extract_first()[:-1]]
+                try:
+                    tval = float(i.xpath('text()').extract_first()[:-1]) if temp=='cost' else eval(temp)[i.xpath('text()').extract_first()]
+                except:
+                    tval = 0
+                curpat[temp] = tval
+            except Exception as e:
+                print('Exception raised: %s'%e)
+                continue
+        curpatdf = curpatdf.append(curpat,ignore_index=True)
+    return curpatdf
+
+
 if 'doctors.data' in os.listdir() and 'doctors_labels.data' in os.listdir():
     with open('doctors.data','rb') as f:
         doctors = pickle.load(f)
@@ -56,19 +123,7 @@ else:
         doctors_labels.append(i[0])
         doctors.append(doctors_on_this_section)
 
-##疗效及态度满意度
-sat_att = sat_eff = {'很不满意':1,'不满意':2,'一般':3,'满意':4,'很满意':5,'其他':0}
-##看病目的
-aim = {'治疗':3,'未填':0,'诊断':2,'其他':1,'咨询问题':4}
-##选择该医生的理由
-reason = {'网上评价':3,'医生推荐':2,'其他':1,'未填':0,'熟人推荐':4,}
-##本次挂号途径
-reservation = {'网络预约':3,'排队挂号':2,'其他':1,'未填':0}
-##目前病情状态
-status = {'有好转':3,'其他':1,'未见好转':2,'痊愈':4,'未填':0}
 
-##名称匹配
-namspc = {'看病目的':'aim','疗效':'sat_eff','态度':'sat_att','选择该医生就诊的理由':'reason','本次挂号途径':'reservation','目前病情状态':'status','本次看病费用总计':'cost'}
 
 
 #从每一个医生界面抓取信息
@@ -84,8 +139,9 @@ for lblix in range(len(doctors)):
         a = this_page.xpath('//td[@class="center orange"]/a/@href').extract_first()
         if a:
             #handling all patient data
-            wc = WebContainer(a)
-            templatel = a.split('.htm'); templatel[1]+='.htm'
+            wc = WebContainer(a,dr)
+            a = wc.xpath('//td[@class="hdf_content"]/div[@class="p_bar"]/a[@class="p_num"]/@href').extract_first()
+            templatel = a.split('2.htm'); templatel[1]+='.htm'
             if len(templatel)!=2: print('Error in handling adress: %s'%a);continue
             curpagnum = 1
             try:
@@ -94,53 +150,11 @@ for lblix in range(len(doctors)):
                 print('Error raised when finding all page numbers on page %s:\n\t%s'%(a,e))
                 continue
             while curpagnum<=totpagnum:
+                if curpagnum not in [1,2]: wc = WebContainer(str(curpagnum).join(templatel),dr)
+                elif curpagnum == 1: wc = this_page
+                patdf = patdf.append(current_page_to_df(wc),ignore_index=True)
+                curpagnum += 1
                 
         else:
-            for pat in this_page.xpath('//table[@class="doctorjy"]'):
-                curpat = pd.Series({'lblix':lblix,'docix':docix},index=['lblix','docix','time','aim','reason','sat_eff','sat_att','reservation','status','cost'])
-                #Time Processing
-                t = pat.xpath('.//td[contains(@style,"text-align:right;")]/text()').extract_first()
-                try: t = parser.parse(t[3:]).strftime(r'%Y-%m-%d')
-                except: t = np.nan
-                curpat['time'] = t
-
-                #dlemd Processing
-                patbriefinfo = pat.xpath('.//td[@class="dlemd"]')
-                ##aim
-                for i in patbriefinfo.xpath('.//td[@colspan="3"]/span/text()').extract():
-                    t = i.split('：')
-                    if t[0] in namspc: 
-                        aim = [eval(namspc[t[0]])[i] if i in eval(namspc[t[0]]) else 1 for i in t[1].split('、')] 
-                        curpat[namspc[t[0]]] = str(aim)
-
-                ##satisfaction
-                t = patbriefinfo.xpath('./table/tbody/tr')[-1].xpath('./td')
-                for i in t[:2]:
-                    try:
-                        text = i.xpath('text()').extract_first()
-                        if text: temp = namspc[text[:-1]]
-                        try:
-                            degr = eval(temp)[i.xpath('span/text()').extract_first()]
-                        except:
-                            degr = 0
-                        curpat[temp] = degr
-                    except Exception as e:
-                        print('Exception raised: %s'%e)
-                        continue
-                
-                #tbody processing
-                patadditinfo = pat.xpath('.//tbody//td[@valign="top"][@height="40px"]')
-                for i in patadditinfo.xpath('div'):
-                    try:
-                        if not i.xpath('span/text()').extract_first(): continue
-                        temp = namspc[i.xpath('span/text()').extract_first()[:-1]]
-                        try:
-                            tval = float(i.xpath('text()').extract_first()[:-1]) if temp=='cost' else eval(temp)[i.xpath('text()').extract_first()]
-                        except:
-                            tval = 0
-                        curpat[temp] = tval
-                    except Exception as e:
-                        print('Exception raised: %s'%e)
-                        continue
-                patdf = patdf.append(curpat,ignore_index=True)
+            patdf = patdf.append(current_page_to_df(this_page),ignore_index=True)
 log.close()
