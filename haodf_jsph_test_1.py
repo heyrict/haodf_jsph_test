d16bf7edd81df8de5c21148b4d494c49a7c8bb5b
heyrict <xiezh0831@126.com>
2017-04-04 22:57:06 +0800

    comments added with minor bugs fixed

--- haodf_jsph_test_1.py
+++ haodf_jsph_test_1.py
@@ -1,14 +1,23 @@
 # coding: utf-8
+## import necessary packages
+### web-climbing libraries
 from selenium import webdriver
 from scrapy import Selector
+### modules for storing data
 import pickle, os, sys
-from datetime import datetime
 import pandas as pd, numpy as np
 from pandas import DataFrame, Series
+### time processing
+from datetime import datetime
 from dateutil import parser
+
+# initial browser
 
 dr = webdriver.PhantomJS()
 dr.set_page_load_timeout(15)
+
+# get web-climbing libraries wrapped
+
 class WebContainer(object):
     def __init__(self,link,PhantomJSDriver=None,logfile=sys.stdout):
         if PhantomJSDriver: self.dr = PhantomJSDriver
@@ -28,11 +37,14 @@ class WebContainer(object):
             print(e)
             print('Failed to scrap %s'%link,file=logfile)
 
+    ## maybe add css version of find() later
     def find(self,expression,kind='xpath'):
         return self.contents.xpath(expression)
 
     def xpath(self,expression):
         return self.find(expression,kind='xpath')
+
+# global variables for labeling data
 
 ##疗效及态度满意度
 sat_att = sat_eff = {'很不满意':2,'不满意':3,'一般':4,'满意':5,'很满意':6,'其他':0,'还不知道':1}
@@ -48,7 +60,7 @@ status = {'有好转':3,'其他':1,'未见好转':2,'痊愈':4,'未填':0}
 namspc = {'看病目的':'aim','疗效':'sat_eff','态度':'sat_att','选择该医生就诊的理由':'reason','本次挂号途径':'reservation','目前病情状态':'status','本次看病费用总计':'cost'}
 
 def current_page_to_df(this_page,logfile,lblix,docix):
-#从医生主页抓取所有患者信息
+    #从医生主页抓取所有患者信息
     if type(this_page)==type(None): return
     global sat_att, sat_eff, aim, reason, reservation, status, namspc, temp
     curpatdf = DataFrame(columns=['lblix','docix','time','aim','reason','sat_eff','sat_att','reservation','status','cost'])
@@ -105,33 +117,40 @@ def current_page_to_df(this_page,logfile,lblix,docix):
 
 def scrape_hospital_page(link,prov_name,hosp_name,logfile):
     #爬取某医院下所有医生主页并返回
+    # initial directory for storing data
     curdir = './%s/%s'%(prov_name,hosp_name)
     if prov_name not in os.listdir(): os.mkdir('%s'%prov_name)
     if hosp_name not in os.listdir('./%s/'%prov_name): os.mkdir('%s'%(prov_name+'/'+hosp_name))
     if 'doctors.data' in os.listdir(curdir) and 'doctors_labels.data' in os.listdir(curdir):
+        # load stored data
         print('Stored index data found in %s'%curdir,file=logfile)
         with open(curdir+'/doctors.data','rb') as f:
             doctors = pickle.load(f)
         with open(curdir+'/doctors_labels.data','rb') as f:
             doctors_labels = pickle.load(f)
     else:
+        # if stored data not found
         wc = WebContainer(link,dr,logfile)
+        ## get all sections
         sections = []
         for i in wc.xpath('//table[@id="hosbra"]//a[@class="blue"]'):
             sections.append((i.xpath('./text()').extract()[0],i.xpath('./@href').extract()[0]))
 
+        ## get doctors in every section
         doctors = []
         doctors_labels = []
         for i in sections:
             wc = WebContainer(i[1],dr,logfile)
+            doctors_on_this_section = [(t.xpath('./text()').extract()[0],t.xpath('./@href').extract()[0]) for t in wc.xpath('//a[@class="name"][@target="_blank"]')]
             try:
                 all_pages = range(1,int(wc.xpath('//a[@class="p_text"][@rel="true"]/text()').extract_first().strip()[1:-1])+1)
             except AttributeError as e:
+                ### if no more than one page exists
                 all_pages = [1]
-                doctors_on_this_section += [(t.xpath('./text()').extract()[0],t.xpath('./@href').extract()[0]) for t in wc.xpath('//a[@class="name"][@target="_blank"]')]
                 doctors_labels.append(i[0])
                 doctors.append(doctors_on_this_section)
                 continue
+
             templatel = wc.xpath('//div[@class="p_bar"]/a[@class="p_num"]/@href').extract_first().split('2.htm')
             try: 
                 templatel[1] = '.htm'+templatel[1]
@@ -139,7 +158,8 @@ def scrape_hospital_page(link,prov_name,hosp_name,logfile):
             except Exception as e:
                 print('Error raised when finding all page numbers on page %s:\n\t%s'%(a,e))
                 continue
-            doctors_on_this_section = [(t.xpath('./text()').extract()[0],t.xpath('./@href').extract()[0]) for t in wc.xpath('//a[@class="name"][@target="_blank"]')]
+
+            ### fine all pages
             for j in all_pages:
                 j = str(j).join(templatel)
                 wc = WebContainer(j,dr,logfile)
