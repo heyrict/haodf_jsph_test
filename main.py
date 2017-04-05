4b33afc939bd108d5fd4e57db14e1d5f27930b06
heyrict <xiezh0831@126.com>
2017-04-05 22:25:13 +0800

    changing logfile to be explicitly called

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from haodf_jsph_test_1 import *
-logfile = open('log.txt','a')
+q = input('Where to store log data?[l(ogfile)/s(tdout)]: ')[0].lower()
+while 1:
+    if q == 'l': logfile = open('log.txt','a');break
+    elif q == 's': import sys;logfile=sys.stdout;break
+    else: print('Cannot recognize input. Check if you entered [l(ogfile)/s(tdout)]')
 print('\n----------\nlog start: %s\n----------'%datetime.today().strftime(r'%Y-%m-%d %H:%M'),file=logfile)
 
 #all_prev: n*2 list
@@ -15,6 +19,7 @@ for prov in all_prov:
         if '%s'%(hosp[0]) not in os.listdir(prov[0]): os.mkdir('%s/%s'%(prov[0],hosp[0]))
         if hosp[0]+'.csv' not in os.listdir(prov[0]):
             scrape_doct_page(doctors,doctors_labels,logfile).to_csv('%s/%s.csv'%(prov[0],hosp[0]),index=False)
+            print('-----End Scraping hosp %d at %s-----'%(hosp[0],datetime.today().strftime(r'%Y-%m-%d %H:%M')),file=logfile)
         else:
             print('%s.csv found in %s/. Skipping...'%(hosp[0],prov[0]))
 
